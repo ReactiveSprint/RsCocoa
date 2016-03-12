cfc5b6458505b0643d6e49a65f97312dcfd54e8f
Ahmad Baraka <ah-baraka@live.com>
2016-03-12 14:44:44 +0200

    Little documentation for FetchedArrayViewModel

--- Pod/Classes/FetchedArrayViewModel.swift
+++ Pod/Classes/FetchedArrayViewModel.swift
@@ -16,15 +16,27 @@ public protocol FetchedArrayViewModelType: class, ArrayViewModelType
     typealias PaginationType
     typealias FetchError: ViewModelErrorType
     
+    /// Whether the ViewModel is refreshing.
     var refreshing: AnyProperty<Bool> { get }
+    
+    /// Whether the ViewModel is fetching next page and is not refreshing.
     var fetchingNextPage: AnyProperty<Bool> { get }
+    
+    /// Whether the ViewModel has next page.
     var hasNextPage: AnyProperty<Bool> { get }
     
+    /// Next Page
     var nextPage: PaginationType? { get }
     
+    /// Action which refreshes ViewModels.
     var refreshAction: Action<FetchInput, [Element], FetchError> { get }
+    
+    /// Action which fetches ViewModels.
+    ///
+    /// If `nextPage` is nil, then this action will refresh, else this action should fetch next page.
     var fetchAction: Action<FetchInput, [Element], FetchError> { get }
     
+    /// Applies fetchAction only if next page is available.
     func fetchIfNeeded(input: FetchInput)
 }
 
@@ -43,6 +55,7 @@ public extension FetchedArrayViewModelType
     }
 }
 
+/// An implementation of FetchedArrayViewModelType that fetches ViewModels by calling `fetchClosure.`
 public class FetchedArrayViewModel<Element: ViewModel, PaginationType, FetchError: ViewModelErrorType>: ViewModel, FetchedArrayViewModelType
 {
     private let _viewModels: MutableProperty<[Element]> = MutableProperty([])
@@ -66,13 +79,19 @@ public class FetchedArrayViewModel<Element: ViewModel, PaginationType, FetchErro
     
     private(set) public var nextPage: PaginationType? = nil
     
-    public let fetchBlock: PaginationType? -> SignalProducer<([Element], PaginationType?), FetchError>
-    private(set) public lazy var refreshAction: Action<(), [Element], FetchError> = self.createRefreshAction()
-    private(set) public lazy var fetchAction: Action<(), [Element], FetchError> = self.createfetchAction()
+    public let fetchClosure: PaginationType? -> SignalProducer<([Element], PaginationType?), FetchError>
+    private(set) public lazy var refreshAction: Action<(), [Element], FetchError> = self.initRefreshAction()
+    private(set) public lazy var fetchAction: Action<(), [Element], FetchError> = self.initFetchAction()
     
-    public init(_ fetchBlock: PaginationType? -> SignalProducer<([Element], PaginationType?), FetchError>)
+    /// Initializes an instance with `fetchClosure`
+    ///
+    /// - Parameter fetchClosure: A closure which is called each time `refreshAction` or `fetchAction`
+    /// are called passing latest PaginationType
+    /// and returns a `SignalProducer` which sends an array of `Element` and PaginationType.
+    /// If the returned SignalProducer sends nil PaginationType, then no pagination will be handled.
+    public init(_ fetchClosure: PaginationType? -> SignalProducer<([Element], PaginationType?), FetchError>)
     {
-        self.fetchBlock = fetchBlock
+        self.fetchClosure = fetchClosure
         super.init()
     }
     
@@ -80,7 +99,13 @@ public class FetchedArrayViewModel<Element: ViewModel, PaginationType, FetchErro
         return _viewModels.value[index]
     }
     
-    public func createRefreshAction() -> Action<(), [Element], FetchError>
+    /// Initializes refresh action.
+    ///
+    /// Default implementation initializes an Action that is enabled when the receiver is,
+    /// and executes `fetchClosure` with nil page.
+    /// 
+    /// The returned action is also bound to the receiver using `bindAction`
+    public func initRefreshAction() -> Action<(), [Element], FetchError>
     {
         let action: Action<(), [Element], FetchError> = Action(enabledIf: self.enabled) { [unowned self] _ in
             self._refreshing.value = true
@@ -92,7 +117,13 @@ public class FetchedArrayViewModel<Element: ViewModel, PaginationType, FetchErro
         return action
     }
     
-    public func createfetchAction() -> Action<(), [Element], FetchError>
+    /// Initializes Fetch action.
+    ///
+    /// Default implementation initializes an Action that is enabled when the receiver is,
+    /// and executes `fetchClosure` with `nextPage`.
+    ///
+    /// The returned action is also bound to the receiver using `bindAction`
+    public func initFetchAction() -> Action<(), [Element], FetchError>
     {
         let action: Action<(), [Element], FetchError> = Action(enabledIf: self.enabled) { [unowned self] _ in
             if self.willFetchNextPage
@@ -113,7 +144,7 @@ public class FetchedArrayViewModel<Element: ViewModel, PaginationType, FetchErro
     
     private func _fetch(page: PaginationType?) -> SignalProducer<[Element], FetchError>
     {
-        return self.fetchBlock(page)
+        return self.fetchClosure(page)
             .on(next: { [unowned self] viewModels, page in
                 if self.refreshing.value
                 {
