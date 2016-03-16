53aed96e086cdf427838d4128ae61d7d8a0e0d53
Ahmad Baraka <ah-baraka@live.com>
2016-03-16 19:03:29 +0200

    Moved iOS specific ViewControllerType implementations to RSUIViewController (to allow NSViewController implementation in the future)

--- Pod/Classes/RSPUIViewController.swift
+++ Pod/Classes/RSPUIViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import ReactiveCocoa
 import Result
 
-/// ViewController which wraps `ViewModel`.
+/// UIViewController which wraps `ViewModel`.
 public class RSPUIViewController<ViewModel: ViewModelType>: UIViewController, ViewControllerType
 {
     /// ViewModel which will be used as context for this "View."
@@ -69,4 +69,63 @@ public class RSPUIViewController<ViewModel: ViewModelType>: UIViewController, Vi
     {
         _presentError(error, viewController: self)
     }
+}
+
+public func _bindActive<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModelType, viewController: ViewController)
+{
+    let presented = RACSignal.merge([
+        viewController.rac_signalForSelector(Selector("viewWillAppear:")).mapReplace(true)
+            .doNext { NSLog("viewWillAppear: %@", $0.description) }
+        , viewController.rac_signalForSelector(Selector("viewWillDisappear:")).mapReplace(false)
+            .doNext { NSLog("viewWillDisappear: %@", $0.description) }
+        ])
+    
+    let appActive = RACSignal.merge([
+        NSNotificationCenter.defaultCenter()
+            .rac_addObserverForName(UIApplicationDidBecomeActiveNotification, object: nil)
+            .mapReplace(true)
+            .doNext { NSLog("UIApplicationDidBecomeActiveNotification: %@", $0.description) }
+        , NSNotificationCenter.defaultCenter()
+            .rac_addObserverForName(UIApplicationWillResignActiveNotification, object: nil)
+            .mapReplace(false)
+            .doNext { NSLog("UIApplicationWillResignActiveNotification: %@", $0.description) }
+        ]).startWith(true)
+        .doNext { NSLog("appActive: %@", $0.description) }
+    
+    let activeSignal = RACSignal.combineLatest([presented, appActive])
+        .and()
+        .toSignalProducer()
+        .map { $0 as! Bool }
+        .flatMapError { _ in SignalProducer<Bool, NoError>.empty }
+    
+    viewModel.active <~ activeSignal
+}
+
+public func _bindTitle<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModel, viewController: ViewController)
+{
+    viewModel.title.producer
+        .takeUntil(viewController.rac_willDeallocSignalProducer())
+        .startWithNext { [unowned viewController] in viewController.title = $0 }
+}
+
+public func _presentLoading<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(loading: Bool, viewController: ViewController)
+{
+    if let loadingView = viewController.loadingView
+    {
+        if loadingView is LoadingViewType
+        {
+            var loadingViewType = loadingView as! LoadingViewType
+            
+            loadingViewType.loading = loading
+        }
+        else
+        {
+            loadingView.hidden = !loading
+        }
+    }
+}
+
+public func _presentError<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(error: ViewModelErrorType, viewController: ViewController)
+{
+    viewController.presentViewController(UIAlertController(error: error), animated: true, completion: nil)
 }
--- Pod/Classes/ViewControllerType.swift
+++ Pod/Classes/ViewControllerType.swift
@@ -58,11 +58,10 @@ public protocol ViewControllerType: class, ViewType
     func presentError(error: ViewModelErrorType)
 }
 
-
 // Composition is required since Swift *currently* doesn't support methods overriding
 // if they were implemented/declared in extensions.
 
-public func _bindViewModel<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModel, viewController: ViewController)
+public func _bindViewModel<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel>(viewModel: ViewModel, viewController: ViewController)
 {
     viewController.bindActive(viewModel)
     viewController.bindTitle(viewModel)
@@ -70,72 +69,13 @@ public func _bindViewModel<ViewModel: ViewModelType, ViewController: ViewControl
     viewController.bindErrors(viewModel)
 }
 
-public func _bindActive<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModelType, viewController: ViewController)
-{
-    let presented = RACSignal.merge([
-        viewController.rac_signalForSelector(Selector("viewWillAppear:")).mapReplace(true)
-            .doNext { NSLog("viewWillAppear: %@", $0.description) }
-        , viewController.rac_signalForSelector(Selector("viewWillDisappear:")).mapReplace(false)
-            .doNext { NSLog("viewWillDisappear: %@", $0.description) }
-        ])
-    
-    let appActive = RACSignal.merge([
-        NSNotificationCenter.defaultCenter()
-            .rac_addObserverForName(UIApplicationDidBecomeActiveNotification, object: nil)
-            .mapReplace(true)
-            .doNext { NSLog("UIApplicationDidBecomeActiveNotification: %@", $0.description) }
-        , NSNotificationCenter.defaultCenter()
-            .rac_addObserverForName(UIApplicationWillResignActiveNotification, object: nil)
-            .mapReplace(false)
-            .doNext { NSLog("UIApplicationWillResignActiveNotification: %@", $0.description) }
-        ]).startWith(true)
-        .doNext { NSLog("appActive: %@", $0.description) }
-    
-    let activeSignal = RACSignal.combineLatest([presented, appActive])
-        .and()
-        .toSignalProducer()
-        .map { $0 as! Bool }
-        .flatMapError { _ in SignalProducer<Bool, NoError>.empty }
-    
-    viewModel.active <~ activeSignal
-}
-
-public func _bindTitle<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModel, viewController: ViewController)
-{
-    viewModel.title.producer
-        .takeUntil(viewController.rac_willDeallocSignalProducer())
-        .startWithNext { [unowned viewController] in viewController.title = $0 }
-}
-
-public func _bindLoading<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModel, viewController: ViewController)
+public func _bindLoading<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel>(viewModel: ViewModel, viewController: ViewController)
 {
     let loadingProducer = viewModel.loading.producer.forwardWhileActive(viewModel)
     loadingProducer.startWithNext(viewController.presentLoading)
 }
 
-public func _presentLoading<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(loading: Bool, viewController: ViewController)
-{
-    if let loadingView = viewController.loadingView
-    {
-        if loadingView is LoadingViewType
-        {
-            var loadingViewType = loadingView as! LoadingViewType
-            
-            loadingViewType.loading = loading
-        }
-        else
-        {
-            loadingView.hidden = !loading
-        }
-    }
-}
-
-public func _bindErrors<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(viewModel: ViewModel, viewController: ViewController)
+public func _bindErrors<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel>(viewModel: ViewModel, viewController: ViewController)
 {
     viewModel.errors.forwardWhileActive(viewModel).observeNext(viewController.presentError)
-}
-
-public func _presentError<ViewModel: ViewModelType, ViewController: ViewControllerType where ViewController.ViewModel == ViewModel, ViewController: UIViewController>(error: ViewModelErrorType, viewController: ViewController)
-{
-    viewController.presentViewController(UIAlertController(error: error), animated: true, completion: nil)
 }
